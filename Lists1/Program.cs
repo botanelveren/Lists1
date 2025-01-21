c3b524edbdd1b46eb528c87b9d1fccbb2a40a0a1
Botan Elveren <botanelveren@gmail.com>
2025-01-21 18:23:31 +0300

    Add project files.

---
+++ Lists1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PatikaPlusGala
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Davetli listesi oluşturma (List kullanımı daha esnektir)
+            List<string> davetliler = new List<string>()
+            {
+                "Bülent Ersoy",
+                "Ajda Pekkan",
+                "Ebru Gündeş",
+                "Hadise",
+                "Hande Yener",
+                "Tarkan",
+                "Funda Arar",
+                "Demet Akalın"
+            };
+
+            // Foreach döngüsü ile listedeki tüm davetlileri ekrana yazdırma
+            Console.WriteLine("Patika Plus Gala Gecesi Davetlileri:");
+            foreach (string davetli in davetliler)
+            {
+                Console.WriteLine(davetli);
+            }
+        }
+    }
+}
